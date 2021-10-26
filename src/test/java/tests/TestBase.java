77238e9140fa2f4df4a83f2e0c0c5ba10ea789f0
rinchy0l0 <rinchyolo@mail.ru>
2021-10-26 23:33:25 +0300

    My first commit

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,51 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import helpers.Attach;
+
+
+public class TestBase {
+
+    public static CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
+    @BeforeAll
+    static void setup() {
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+
+        String url = System.getProperty("url");
+
+        String login = credentials.login();
+        String password = credentials.password();
+
+        String remote = "https://" + login + ":" + password + "@" + url;
+
+        Configuration.remote = remote;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
